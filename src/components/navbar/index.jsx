52284bfff44880187a1814967aad4bea47761f84
yuuchien.yang@gmail.com <yuuchien.yang@gmail.com>
2023-08-04 23:14:48 +0800

    fixed nav bar

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,13 +1,7 @@
 import avatar from "assets/img/avatars/avatar4.png";
-import navbarimage from "assets/img/layout/Navbar.png";
 import Dropdown from "components/dropdown";
 import React from "react";
-import { BsArrowBarUp } from "react-icons/bs";
-import { FiAlignJustify, FiSearch } from "react-icons/fi";
-import {
-  IoMdInformationCircleOutline,
-  IoMdNotificationsOutline,
-} from "react-icons/io";
+import { FiAlignJustify } from "react-icons/fi";
 import { RiMoonFill, RiSunFill } from "react-icons/ri";
 import { Link } from "react-router-dom";
 
@@ -46,8 +40,8 @@ const Navbar = (props) => {
         </p>
       </div>
 
-      <div className="relative mt-[3px] flex h-[61px] w-[355px] flex-grow items-center justify-around gap-2 rounded-full bg-white px-2 py-2 shadow-xl shadow-shadow-500 dark:!bg-navy-800 dark:shadow-none md:w-[365px] md:flex-grow-0 md:gap-1 xl:w-[365px] xl:gap-2">
-        <div className="flex h-full items-center rounded-full bg-lightPrimary text-navy-700 dark:bg-navy-900 dark:text-white xl:w-[225px]">
+      <div className="relative mt-[3px] flex h-[61px] w-48 flex-grow items-center justify-around gap-2 rounded-full bg-white px-2 py-2 shadow-xl shadow-shadow-500 dark:!bg-navy-800 dark:shadow-none md:w-48 md:flex-grow-0 md:gap-1 xl:w-32 xl:gap-2">
+        {/* <div className="flex h-full items-center rounded-full bg-lightPrimary text-navy-700 dark:bg-navy-900 dark:text-white xl:w-[225px]">
           <p className="pl-3 pr-2 text-xl">
             <FiSearch className="h-4 w-4 text-gray-400 dark:text-white" />
           </p>
@@ -56,7 +50,7 @@ const Navbar = (props) => {
             placeholder="Search..."
             class="block h-full w-full rounded-full bg-lightPrimary text-sm font-medium text-navy-700 outline-none placeholder:!text-gray-400 dark:bg-navy-900 dark:text-white dark:placeholder:!text-white sm:w-fit"
           />
-        </div>
+        </div> */}
         <span
           className="flex cursor-pointer text-xl text-gray-600 dark:text-white xl:hidden"
           onClick={onOpenSidenav}
@@ -64,7 +58,7 @@ const Navbar = (props) => {
           <FiAlignJustify className="h-5 w-5" />
         </span>
         {/* start Notification */}
-        <Dropdown
+        {/* <Dropdown
           button={
             <p className="cursor-pointer">
               <IoMdNotificationsOutline className="h-4 w-4 text-gray-600 dark:text-white" />
@@ -112,9 +106,9 @@ const Navbar = (props) => {
             </div>
           }
           classNames={"py-2 top-4 -left-[230px] md:-left-[440px] w-max"}
-        />
+        /> */}
         {/* start Horizon PRO */}
-        <Dropdown
+        {/* <Dropdown
           button={
             <p className="cursor-pointer">
               <IoMdInformationCircleOutline className="h-4 w-4 text-gray-600 dark:text-white" />
@@ -155,7 +149,8 @@ const Navbar = (props) => {
           }
           classNames={"py-2 top-6 -left-[250px] md:-left-[330px] w-max"}
           animation="origin-[75%_0%] md:origin-top-right transition-all duration-300 ease-in-out"
-        />
+        /> */}
+        {/* start Dark mode */}
         <div
           className="cursor-pointer text-gray-600"
           onClick={() => {
